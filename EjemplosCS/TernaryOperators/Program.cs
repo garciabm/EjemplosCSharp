1890085831d547e7c430980e1f6cdcd13c7e66c8
Miguel <garcia.bonilla.m@gmail.com>
2016-11-02 00:53:49 -0600

    Cambio

--- EjemplosCS/TernaryOperators/Program.cs
+++ EjemplosCS/TernaryOperators/Program.cs
@@ -10,6 +10,58 @@ namespace TernaryOperators
     {
         static void Main(string[] args)
         {
+            string variableCadena = null;
+            string resultado = string.Empty;
+
+            resultado = EjemploIf(variableCadena);
+            //resultado = EjemploTernary(variableCadena);
+            //resultado = EjemploTernaryDoble(variableCadena);
+
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+
+            variableCadena = "Algo";
+            resultado = string.Empty;
+
+
+            resultado = EjemploIf(variableCadena);
+            //resultado = EjemploTernary(variableCadena);
+            //resultado = EjemploTernaryDoble(variableCadena);
+
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+
+
         }
+
+        public static string EjemploIf(string cadena)
+        {
+            string resultado;
+            if (cadena == null)
+            {
+                resultado = "cadena es nula";
+            }
+            else
+            {
+                resultado = cadena;
+            }
+            return resultado;
+        }
+
+        public static string EjemploTernary(string cadena)
+        {
+            string resultado;
+            resultado = cadena == null ? "cadena es nula" : cadena;
+            return resultado;
+
+        }
+
+        public static string EjemploTernaryDoble(string cadena)
+        {
+            string resultado;
+            resultado = cadena ?? "cadena es nula";
+            return resultado;
+        }
+       
     }
 }
