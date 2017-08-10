f3ba32d6bba475151aaaad7d327f63032a086e4c
Mike <garcia.bonilla.m@gmail.com>
2017-08-10 17:42:02 -0500

    Avance ejemplo

---
+++ EjemplosCS/SimpleTask/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTask
+{
+    public class Producto
+    {
+        public TimeSpan TiempoElaboracion { get; set; }
+        public string Nombre { get; set; }
+        public int Opcion { get; set; }
+    }
+}
---
+++ EjemplosCS/SimpleTask/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleTask
+{
+    class Program
+    {
+        static List<Producto> productos = new List<Producto>();
+        static int orden = 1; 
+
+        static void DefineMenu()
+        {
+            productos.Add(new Producto { TiempoElaboracion = TimeSpan.FromSeconds(7), Nombre = "Hamburguesa", Opcion = 1 });
+            productos.Add(new Producto { TiempoElaboracion = TimeSpan.FromSeconds(3), Nombre = "HotDog", Opcion = 2 });
+        }
+
+        static void Main(string[] args)
+        {
+            DefineMenu();
+            bool activo = true;
+            List<Task> tasks = new List<Task>();
+            while (activo)
+            {
+                Console.WriteLine("¿Que desea Ordenar?");
+                string opcion = Console.ReadLine();
+                if (opcion == "c")
+                {
+                    activo = false;
+                    Console.WriteLine("Esperando todas las ordenes");
+                    Task.WaitAll(tasks.ToArray());
+                    Console.WriteLine("Hasta Luego");
+                }
+                else
+                {
+                    Task task = Task.Factory.StartNew(() => Ejecuta(Convert.ToInt32(opcion)));
+                    tasks.Add(task);
+                    //Ejecuta(Convert.ToInt32(opcion));
+                }
+            }
+        }
+
+        public static void Ejecuta(int valor)
+        {
+            Producto producto = productos.FirstOrDefault(c => c.Opcion == valor);
+            if (producto != null)
+            {
+                int ordenAsignada = orden;
+                orden++;
+                Console.WriteLine(string.Format("Preparando {0} orden {1}", producto.Nombre, ordenAsignada));
+                Thread.Sleep(producto.TiempoElaboracion);
+                Console.WriteLine(string.Format("Orden {0} lista, sale {1}",ordenAsignada, producto.Nombre));
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Opción no Valida");
+            }
+        }
+    }
+}
