eb8f6aac3ed78d0912d6a85df229517b04352438
mariodotnet <contactomariodotnet@gmail.com>
2017-11-28 23:37:02 -0500

    Agregando ejemplo de arreglo

---
+++ EjemplosCS/ArrayCSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instancia de arreglo de enteros
+            int[] arreglo = new int[5];
+
+            //asignar valor a cada indice (empezando por 0)
+            arreglo[0] = 10;
+            arreglo[1] = 20;
+            arreglo[2] = 30;
+            arreglo[3] = 40;
+            arreglo[4] = 50;
+
+            //iterar arreglo e imprimir valores
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.WriteLine(arreglo[i]);
+            }
+
+            //pausa en pantalla
+            Console.ReadKey();
+
+        }
+    }
+}
